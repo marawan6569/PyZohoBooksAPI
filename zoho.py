744f45edaffd09915e3c3eefb56a181057e28db6
marawan6569 <marawan6569@gmail.com>
2022-03-24 12:45:17 +0200

    updated README.md

--- zoho.py
+++ zoho.py
@@ -5,11 +5,11 @@ from PyZohoBooksAPI import ZohoAuth, Contacts, Invoices
 #     client_secret='ab74c6e5565fd99fdee9e40b364cd960941c4529da'
 # )
 
-# print(auth.generate_token(code='1000.67f3c4f5ae1406fe584aef60603b8ced.1fc7b8bf6fd67f5fa757a3c1a40a3008'))
+# print(auth.generate_token(code='1000.c8a9c216a67f7ae988f3c706a547dfa4.9b799eda80b7839310ef35c56e46bb23'))
 # print(auth.refresh_token(refresh_token='1000.455b2011ed031afa48df852cfcfe7f9b.0aeb72a01ad1841ccbf5df8e548f9445'))
 
 
-# contacts = Contacts(token='1000.974b30cc15660e53a11904fca98066ea.607e25ecd3f226ff0484960dd020e74e',
+# contacts = Contacts(token='1000.ce1d8d22982db8d5c80ea8c8903ca458.b53a0d99b4b0325f4ff554a8a452a92b',
 #                     organization_id='774525475')
 
 # _, cl = contacts.contacts_list()
